a17c4af82b5c9f95b4bcd44224252b2ea41ef1b5
James Coulter <seniordev1994128@gmail.com>
2022-04-29 11:20:54 -0700

    config for broadcasting and redis service

--- config/database.php
+++ config/database.php
@@ -93,6 +93,15 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'redis' => [
+            'client' => 'predis',
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => env('REDIS_DATABASE', 0),
+            ],
+        ],
     ],
 
     /*
